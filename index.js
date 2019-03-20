7abc043157b875758eb10acae4c0c098f87610b0
Sujit <sujitr@google.com>
2019-03-19 23:21:06 -0700

    added exception handling

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const app = express()
 app.use(bodyParser.json())
 
 await storage.init();
-app.get('/', async (req, res) => {
-  res.send(await storage.getItem('votes'));
-})
-
 app.post('/', async (req, res) => {
-  const votes = await storage.getItem('votes');
+  try {
+    const votesStoreKey = 'votes';
+    const votes = await storage.getItem(votesStoreKey);
 
-  for (var key in Object.keys(req.body)) {
-    votes[key] = votes[key] ? votes[key] + 1 : 1;
+    for (var key in Object.keys(req.body)) {
+      votes[key] = votes[key] ? votes[key] + 1 : 1;
+    }
+  
+    await storage.setItem(votesStoreKey, votes);
+    res.send(votes);
+  } catch(e) {
+    res.status(500).send(e.message);
   }
-
-  await storage.setItem('votes', votes);
-  res.send('vote received');
 })
 
 app.listen(8080, () => console.log('Nodejs Voting service listening on port 8080!'))
