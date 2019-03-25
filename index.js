89609f9ce1a68e4bef99f404bade4c07178c21a5
Sujit <sujitr@google.com>
2019-03-24 22:38:53 -0700

    Adding request logs for voting service

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const storage = require('node-persist');
+const morgan = require('morgan');
 
+// configure middleware
 const app = express()
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+morgan.token('body', (req, res) => { return req.body ? JSON.stringify(req.body) : ''; });
+app.use(morgan('HTTP/:http-version :method :url :body :status'));
 
+// storage
 await storage.init();
 const votesStoreKey = 'votes';
 
